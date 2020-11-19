a567cbac024ff10ce34e4ac6b48491a738e09060
Michael Ho <twho@umich.edu>
2020-11-18 21:47:27 -0800

    Project init

---
+++ InterviewPractices/InterviewPractices/JSONParsingExample/ListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  InterviewPractices
+//
+//  Created by Michael Ho on 11/18/20.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
