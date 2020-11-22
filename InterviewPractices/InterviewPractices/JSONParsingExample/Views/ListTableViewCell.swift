7dc9ccc64e51158f2af432205c68c402a08854b4
Michael Ho <twho@umich.edu>
2020-11-22 14:10:14 -0800

    Complete ListViewController

--- InterviewPractices/InterviewPractices/JSONParsingExample/Views/ListTableViewCell.swift
+++ InterviewPractices/InterviewPractices/JSONParsingExample/Views/ListTableViewCell.swift
@@ -12,23 +12,31 @@ class ListTableViewCell: UITableViewCell {
     private var restaurantImageView: UIImageView!
     private var title: UILabel!
     private var rating: RatingView!
+    private var priceLabel: UILabel!
+    private var phoneLabel: UILabel!
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.restaurantImageView = UIImageView()
         self.restaurantImageView.setCornerBorder()
+        self.rating = RatingView()
         self.title = UILabel()
         self.title.font = .boldSystemFont(ofSize: self.title.font.pointSize)
         self.title.numberOfLines = 2
-        self.title.adjustsFontSizeToFitWidth = true
-        self.rating = RatingView()
-        self.addSubViews([restaurantImageView, title, rating])
+        self.priceLabel = UILabel()
+        self.phoneLabel = UILabel()
+        [title, priceLabel, phoneLabel].forEach {
+            $0.adjustsFontSizeToFitWidth = true
+        }
+        self.addSubViews([restaurantImageView, title, rating, priceLabel, phoneLabel])
     }
     
     func loadDataToView(_ restaurant: Restaurant) {
         self.restaurantImageView.loadImage(restaurant.thumbnail)
         self.title.text = restaurant.name
         self.rating.value = restaurant.rating
+        self.priceLabel.text = "$\(restaurant.priceRange.components(separatedBy: ",")[0]) - \(restaurant.priceRange.components(separatedBy: ",")[1])"
+        self.phoneLabel.text = "#\(restaurant.phoneNumber)"
     }
     
     override func layoutSubviews() {
@@ -38,13 +46,17 @@ class ListTableViewCell: UITableViewCell {
         self.restaurantImageView.setConstraintsToView(top: self, tConst: 0.1 * height,
                                                       bottom: self, bConst: -0.1 * height,
                                                       left: self, lConst: 0.05 * width)
-        self.restaurantImageView.widthAnchor.constraint(equalTo: self.restaurantImageView.heightAnchor).isActive = true
+        self.restaurantImageView.widthAnchor.constraint(equalTo: restaurantImageView.heightAnchor).isActive = true
         self.title.setConstraintsToView(top: self, tConst: 0.1 * height, right: self, rConst: -0.05 * width)
         self.title.leftAnchor.constraint(equalTo: restaurantImageView.rightAnchor, constant: 0.05 * width).isActive = true
         self.rating.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 0.05 * height).isActive = true
         self.rating.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.1).isActive = true
         self.rating.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -0.05 * width).isActive = true
-        self.rating.leftAnchor.constraint(equalTo: self.restaurantImageView.rightAnchor, constant: 0.05 * width).isActive = true
+        self.rating.leftAnchor.constraint(equalTo: restaurantImageView.rightAnchor, constant: 0.05 * width).isActive = true
+        self.priceLabel.topAnchor.constraint(equalTo: rating.bottomAnchor, constant: 0.05 * height).isActive = true
+        self.priceLabel.setConstraintsToView(left: rating, right: rating)
+        self.phoneLabel.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: 0.05 * height).isActive = true
+        self.phoneLabel.setConstraintsToView(left: rating, right: rating)
         self.layoutIfNeeded()
     }
     
