d4f921d67cbabd5339f05b780d70e4d9420a5fb5
Michael Ho <twho@umich.edu>
2020-11-23 18:24:19 -0800

    Fix stack view bug

--- InterviewPractices/MenuViewController.swift
+++ InterviewPractices/MenuViewController.swift
@@ -13,27 +13,16 @@ class MenuViewController: UIViewController {
     private var stackView: UIStackView!
     // Data
     private let examples: [Example] = [.imageLoader, .jsonParser]
-
+    // viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    override func loadView() {
-        super.loadView()
         self.view.backgroundColor = .white
         let title = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? "Interview Practices"
         navbar = self.addNavigationBar(title: title)
-        let instructions = UILabel(title: "Select one of the following examples", size: 18.0, color: .black, numOfLines: 2)
-        stackView = UIStackView(arrangedSubviews: [instructions], axis: .vertical, distribution: .fillEqually, spacing: 0.025 * self.view.frame.height)
-        // Setup buttons
-        for example in examples {
-            let button = MHButton(example: example)
-            button.addTarget(self, action: #selector(buttonClicked(_:)), for: .touchUpInside)
-            stackView.addArrangedSubview(button)
-        }
+        stackView = UIStackView(axis: .vertical, distribution: .fillEqually, spacing: 0.025 * self.view.frame.height)
         self.view.addSubViews([stackView])
     }
-    
+    // viewDidLayoutSubviews
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         let width = self.view.frame.width
@@ -43,7 +32,26 @@ class MenuViewController: UIViewController {
         stackView.setHeightConstraint(CGFloat(self.examples.count + 1) * 100.0)
         self.view.layoutIfNeeded()
     }
-    
+    // viewDidAppear
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if stackView.subviews.count != self.examples.count + 1 {
+            stackView.removeAllSubviews()
+            let instructions = UILabel(title: "Select one of the following examples", size: 18.0, color: .black, numOfLines: 2)
+            stackView.addArrangedSubview(instructions)
+            // Setup buttons
+            for example in examples {
+                let button = MHButton(example: example)
+                button.addTarget(self, action: #selector(buttonClicked(_:)), for: .touchUpInside)
+                stackView.addArrangedSubview(button)
+            }
+        }
+    }
+    /**
+     Handle button click event. Present the example view controller user clicked.
+     
+     - Parameter sender: The button clicked by the user.
+     */
     @objc private func buttonClicked(_ sender: UIButton) {
         if let exampleButton = sender as? MHButton {
             let vcToPresent = exampleButton.example.viewController
